906ab587b8ff7b19f4c81b0919d9fffe4990982a
selam lemma <oneiconforlife26@gmail.com>
2020-08-31 14:40:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def name (fido)
-    @the_dog = fido
+  def name (dog_name)
+    @the_dog = dog_name
 end
-def name=(fido)
+def name=(dog_name)
   @the_dog
 end
-def breed=(fido)
+def breed=(dog_name)
   puts "breed"
 end
 end
