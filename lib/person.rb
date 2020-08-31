318da6d5d48e4e4dbbdb7895269206ec5e82df0c
selam lemma <oneiconforlife26@gmail.com>
2020-08-31 14:36:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
-  def name (Beyonce)
-    @the_person = Beyonce
+  def name (person_name)
+    @the_person = Person_name
 end
 def name
   @the_person
 end
-def job=(Beyonce)
+def job=(dog_name)
   puts "job"
 end
 end
